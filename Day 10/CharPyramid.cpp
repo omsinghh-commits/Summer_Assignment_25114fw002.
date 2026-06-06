c30defdd5a920d3ecfef607b70e79bebe49dbb90
Om Singh <omsingh02082007@gmail.com>
2026-06-06 23:06:33 +0530

    Print character pyramid pattern

---
+++ Day 10/CharPyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+cout << "ENTER THE NUMBER OF ROWS : ";
+cin >> n;
+
+for(int i=1; i<=n; i++){
+    for(int j=1; j<= n-i; j++){
+        cout << " ";
+    }
+    for(int j=0; j<i; j++){
+        cout << (char)('A'+ j);
+    }
+    for(int j=i-2; j>=0; j--){
+        cout << (char)('A'+ j);
+    }
+    cout << endl;}
+    return 0;
+}
