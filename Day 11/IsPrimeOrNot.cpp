dfeb125fe331ba7c9084a5e4f46b33a8ea00d232
Om Singh <omsingh02082007@gmail.com>
2026-06-07 17:30:06 +0530

    Function to check prime

---
+++ Day 11/IsPrimeOrNot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "ENTER A NUMBER FOR CHECKING PRIME OR NOT : ";
+    cin >> n;
+    if (isPrime(n)){
+        cout << n << " IS PRIME " << endl;
+    }else{
+        cout << n << " IS NOT PRIME " << endl;
+    }
+
+    return 0;
+}
