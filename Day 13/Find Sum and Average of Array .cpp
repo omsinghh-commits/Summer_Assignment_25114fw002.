18ccbd6047bd8814b0bfcd2f1672b66fb7bfc5ba
Om Singh <omsingh02082007@gmail.com>
2026-06-09 22:39:07 +0530

    Create Find Sum and Average of Array .cpp

---
+++ Day 13/Find Sum and Average of Array .cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    double average = (double)sum / n;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average;
+
+    return 0;
+}
