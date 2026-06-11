390a7e9485599213d2fe405e37d80d0f171e369f
Om Singh <omsingh02082007@gmail.com>
2026-06-11 21:33:26 +0530

    Rotate an array right

---
+++ Day 15/RotateArrayRight.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cout << "ENTER SIZE OF ARRAY : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "ENTER ELEMENTS : ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "ENTER NUMBER OF RIGHT ROTATIONS (k): ";
+    cin >> k;
+
+    k = k % n; 
+
+    int start = 0, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    start = 0, end = k - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    start = k, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    cout << "ARRAY AFTER ROTATIONS : ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;}
