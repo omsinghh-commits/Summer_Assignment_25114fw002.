67291cd7ec45660dfd1cf7eb332bbe3cf4e09dd0
Om Singh <omsingh02082007@gmail.com>
2026-06-14 20:38:09 +0530

    Program to selection sort

---
+++ Day 18/SelectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "BEFORE SORTING : ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;  
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    cout << "AFTER SORTING :  ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;}
